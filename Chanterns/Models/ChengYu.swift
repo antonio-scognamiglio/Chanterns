2de6b98665401846337f3844f4c4bf4b3b10e8f4
Antonio Scognamiglio <tonyscogna95@gmail.com>
2023-04-14 22:18:58 +0200

    Added portrait images

--- Chanterns/Models/ChengYu.swift
+++ Chanterns/Models/ChengYu.swift
@@ -17,12 +17,14 @@ struct ChengYu: Identifiable {
     // array of 4 characters
     var id = UUID()
     var arrayCharacters: [ChineseCharacter]
+    var complete: String
     var meaning: String
+    
     
     static let example =  ChengYu(arrayCharacters: [ChineseCharacter(hanzi: "开", pinyin: "kāi", isGuessed: false),
                                                     ChineseCharacter(hanzi: "门", pinyin: "mén", isGuessed: false),
                                                     ChineseCharacter(hanzi: "见", pinyin: "jiàn", isGuessed: false),
                                                     ChineseCharacter(hanzi: "山", pinyin: "shān", isGuessed: false)],
-                                   meaning: "Come straight to the point; without preamble")
+                                  complete: "开门见山", meaning: "Come straight to the point; without preamble")
 }
 
--- Chanterns/Views/ChengYuView.swift
+++ Chanterns/Views/ChengYuView.swift
@@ -10,10 +10,36 @@ import SwiftUI
 struct ChengYuView: View {
     @State var chengYu: ChengYu
     
+    var scrollImages = ["ScrollClose", "ScrollSemiOpen", "ScrollOpen"]
+    @State var index = 0
+    @State var showScrollClose = true
+    @State var startAnimation = false
+    @State var showChengYu = false
+//    @State var showScrollSemi = false
+//    @State var showScrollOpen = false
+    
     var body: some View {
         ZStack {
-            Image("ScrollOpen")
-            
+            Image(scrollImages[index])
+                .resizable()
+                .scaledToFit()
+                .frame(width: UIScreen.main.bounds.width / 2)
+//                .animation(.linear(duration: 0.2), value: index)
+
+            Text(showChengYu ? chengYu.complete : "")
+                .font(.system(size: 96))
+        }
+        .onAppear {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.3 , execute: {
+             index += 1
+            })
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.6 , execute: {
+             index += 1
+            })
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.9 , execute: {
+                showChengYu = true
+            })
+          
         }
     }
 }
