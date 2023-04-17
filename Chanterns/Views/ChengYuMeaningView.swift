0059f736b24094853f6e194387fe1985d6e0d00b
Antonio Scognamiglio <tonyscogna95@gmail.com>
2023-04-17 19:40:29 +0200

    Added meaningView

--- Chanterns/Views/ChengYuMeaningView.swift
+++ Chanterns/Views/ChengYuMeaningView.swift
@@ -8,13 +8,45 @@
 import SwiftUI
 
 struct ChengYuMeaningView: View {
+    var chengYu: ChengYu
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        GeometryReader { geo in
+            ZStack {
+                Image("ScrollOpen")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: geo.size.width * 0.85)
+                    .shadow(radius: 5)
+                    .overlay {
+                        VStack{
+                            Text(chengYu.complete)
+                                .font(.system(size: 64))
+                                
+                            HStack {
+                                ForEach(chengYu.arrayCharacters){ character in
+                                    Text(character.pinyin)
+                                        .font(.system(size: 32))
+                                }
+                            }
+                            .padding(.bottom, 1)
+                                
+                                Text(chengYu.meaning)
+                                    .font(.system(size: 34))
+                                    .frame(width: geo.size.width * 0.65, alignment: .leading)
+//                                    .padding(.top, 1)
+                            Spacer()
+                            }
+                        .frame(height: geo.size.height * 0.2)
+                        }
+                    }
+            .frame(width: geo.size.width, height: geo.size.height)
+        }
     }
 }
 
 struct ChengYuMeaningView_Previews: PreviewProvider {
     static var previews: some View {
-        ChengYuMeaningView()
+        ChengYuMeaningView(chengYu: ChengYu.example)
     }
 }
